8e4e0ffdd335ee11baa727e544c800ad3e73aca5
Alexey Buzdin <alexey.buzdin@ctco.lv>
2016-02-17 03:12:18 +0200

    Fixed #15 Configured cukes to run on mvn:test; Test failures detected

--- cukes-rest-sample/src/test/java/lv/ctco/cukesrest/run/RunCukesTest.java
+++ cukes-rest-sample/src/test/java/lv/ctco/cukesrest/run/RunCukesTest.java
@@ -1,16 +1,22 @@
 package lv.ctco.cukesrest.run;
 
+import cucumber.api.*;
 import cucumber.api.junit.*;
 import lv.ctco.cukesrest.*;
 import org.junit.*;
 import org.junit.runner.*;
 
-@Ignore
 @RunWith(Cucumber.class)
+@CucumberOptions(
+    format = { "pretty" },
+    features = "classpath:features",
+    glue = "lv.ctco.cukesrest.api",
+    strict = true
+)
 public class RunCukesTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
         new SampleApplication().run(new String[]{"server", "server.yml"});
     }
 }
