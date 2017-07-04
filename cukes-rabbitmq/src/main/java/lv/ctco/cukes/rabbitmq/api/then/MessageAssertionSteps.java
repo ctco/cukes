bd9cc9125bbb69a68c1ac7d44e631d53e52d33eb
Sergey Trasko <sergey.trasko@ctco.lv>
2017-07-04 16:49:28 +0300

    Add JSON support #9 -- Step definitions added

--- cukes-rabbitmq/src/main/java/lv/ctco/cukes/rabbitmq/api/then/MessageAssertionSteps.java
+++ cukes-rabbitmq/src/main/java/lv/ctco/cukes/rabbitmq/api/then/MessageAssertionSteps.java
@@ -14,6 +14,11 @@ public class MessageAssertionSteps {
     @Inject
     ResponseFacade responseFacade;
 
+    @Then("^let variable \"(.+)\" equal to message body")
+    public void variableAssignedFromMessageBody(String varName) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
     @Then("^message body is empty$")
     public void assertMessageBodyIsEmpty() {
         throw new NotImplementedException("Not yet implemented");
@@ -41,7 +46,7 @@ public class MessageAssertionSteps {
 
     @Then("^message body does not contain \"(.+)\"$")
     public void assertMessageBodyDoesNotContain(String body) {
-            throw new NotImplementedException("Not yet implemented");
+        throw new NotImplementedException("Not yet implemented");
     }
 
     @Then("^message body contains property \"(.+)\" containing phrase \"(.*)\"$")
@@ -52,6 +57,46 @@ public class MessageAssertionSteps {
     @Then("^message body contains property \"(.+)\" with value \"(.*)\"$")
     public void assertMessageContainsPropertyWithValue(String path, String value) {
         responseFacade.assertMessageContainsPropertyWithValue(path, value);
+    }
+
+    @Then("^message body contains property \"(.+)\" with value other than \"(.*)\"$")
+    public void assertMessageBodyContainsPathWithOtherValue(String path, String value) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains property \"(.+)\" of type \"(.+)\"$")
+    public void assertMessageBodyContainsPathOfType(String path, String type) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains an array \"(.+)\" of size (>=|>|<=|<|<>) (\\d+)$")
+    public void assertMessageBodyContainsArrayWithSize(String path, String operator, Integer size) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains an array \"(.+)\" of size (\\d+)$")
+    public void assertMessageBodyContainsArrayWithSize(String path, Integer size) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains an array \"(.+)\" with value \"(.*)\"$")
+    public void assertMessageBodyContainsArrayWithEntryHavingValue(String path, String value) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body does not contain property \"(.+)\"")
+    public void assertMessageBodyDoesNotContainPath(String path) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains property \"(.+)\" matching pattern \"(.+)\"$")
+    public void assertMessageBodyContainsPathMatchingPattern(String path, String pattern) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Then("^message body contains property \"(.+)\" not matching pattern \"(.+)\"$")
+    public void assertMessageBodyContainsPathNotMatchingPattern(String path, String pattern) {
+        throw new NotImplementedException("Not yet implemented");
     }
 
 }
