8c70c622e0d990c2c22ac2f39ffee34d9f5bd18f
Igor Gursky <Igor_Gursky@rcomext.com>
2017-11-24 11:44:34 +0200

    Fixed issue with CukesHttpPlugins not executing full workflow

--- cukes-graphql/src/main/java/lv/ctco/cukes/graphql/CukesGraphQLGuiceModule.java
+++ cukes-graphql/src/main/java/lv/ctco/cukes/graphql/CukesGraphQLGuiceModule.java
@@ -3,15 +3,15 @@ package lv.ctco.cukes.graphql;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import lv.ctco.cukes.core.extension.CukesInjectableModule;
+import lv.ctco.cukes.core.extension.CukesPlugin;
 import lv.ctco.cukes.graphql.internal.PreprocessGraphQLRequestBody;
-import lv.ctco.cukes.http.extension.CukesHttpPlugin;
 
 @CukesInjectableModule
 public class CukesGraphQLGuiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        Multibinder<CukesHttpPlugin> multibinder = Multibinder.newSetBinder(binder(), CukesHttpPlugin.class);
+        Multibinder<CukesPlugin> multibinder = Multibinder.newSetBinder(binder(), CukesPlugin.class);
         multibinder.addBinding().to(PreprocessGraphQLRequestBody.class);
     }
 }
